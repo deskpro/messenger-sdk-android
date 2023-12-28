1332b76d4c6684eeaf29019798ecea6929f05ccd
Nikola Divljan <nikola.divljan@qsd.ba>
2023-12-28 23:52:01 +0100

    chore: Update compileSdk version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.deskpro.messengersdkandroid"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.deskpro.messengersdkandroid"
