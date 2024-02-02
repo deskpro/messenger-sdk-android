38a29d4f95ccaabc4d75790e55a63b329f9cbe3c
Igor_QSD <info@qsdbih.com>
2024-02-02 11:21:28 +0100

    Add simple log tracker, Add app id to UI, Validate forms on opening deskpro messenger, Add notification permission flow, Add functions to json parsing user object

--- app/src/main/java/com/deskpro/messengersdkandroid/MainActivity.kt
+++ app/src/main/java/com/deskpro/messengersdkandroid/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.deskpro.messengersdkandroid
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
+import androidx.lifecycle.lifecycleScope
 import com.deskpro.messenger.DeskPro
 import com.deskpro.messenger.data.MessengerConfig
-import com.deskpro.messenger.data.PushNotificationData
 import com.deskpro.messenger.data.User
 import com.deskpro.messengersdkandroid.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Main activity hosting the DeskPro Messenger integration demonstration.
@@ -20,6 +33,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     private var messenger: DeskPro? = null
     private var appUrl = "https://dev-pr-12730.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d"
+    private var appId = ""
     private var jwtToken = ""
     private var user: User? = null
     private var userInfo = "{\n" +
@@ -28,6 +42,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             "  \"last_name\": \"Doe\",\n" +
             "  \"email\": \"john.doe@mail.com\"\n" +
             "}"
+
+    private val stringBuilder = StringBuilder()
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            Log.d(TAG, "Notification permission granted!")
+        } else {
+            Log.w(TAG, "Notification permission denied!")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,58 +63,109 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         binding.etUrl.setText(appUrl)
         binding.etUserInfo.setText(userInfo)
+        binding.tvEvents.movementMethod = ScrollingMovementMethod()
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            Runtime.getRuntime().exec("logcat -c") // Clear logs
+            Runtime.getRuntime().exec("logcat") // Start to capture new logs
+                .inputStream
+                .bufferedReader()
+                .useLines { lines ->
+                    // Note that this forEach loop is an infinite loop until this job is cancelled.
+                    lines.forEach { newLine ->
+                        // Check whether this job is cancelled, since a coroutine must
+                        // cooperate to be cancellable.
+                        ensureActive()
+                        if (newLine.contains("DeskPro")) {
+                            stringBuilder.append("<font color='blue'>$newLine</font><br>")
+                        }
+                        if (newLine.contains("chromium")) {
+                            stringBuilder.append("<font color='purple'>$newLine</font><br>")
+                        }
+                        withContext(Dispatchers.Main) {
+                            binding.tvEvents.text = HtmlCompat.fromHtml(stringBuilder.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+                        }
+                    }
+                }
+        }
     }
 
     override fun onClick(v: View?) {
         binding.apply {
             when (v) {
-                btnUrl -> {
-                    appUrl = etUrl.text.toString()
-                }
-
-                btnJWT -> {
-                    jwtToken = etJWT.text.toString()
-                    messenger?.authorizeUser(jwtToken)
-                }
-
-                btnUserInfo -> {
-                    userInfo = etUserInfo.text.toString()
-                    //TODO: parse userInfo string and set user info
-                    user = User()
-                }
-
                 btnNotifications -> {
-                    //TODO check for permission
+                    askNotificationPermission()
                     //messenger?.handlePushNotification(PushNotificationData())
                 }
 
                 btnOpenMessenger -> {
-                    messenger = DeskPro(MessengerConfig(appUrl = appUrl, ""))
-                    messenger?.initialize(applicationContext)
-                    messenger?.present()?.show()
-                    user?.let {
-                        messenger?.setUserInfo(it)
+                    appUrl = etUrl.text.toString()
+                    appId = etAppId.text.toString()
+                    jwtToken = etJWT.text.toString()
+                    userInfo = etUserInfo.text.toString()
+
+                    if (appUrl.isEmpty() || !appUrl.contains("https://")) {
+                        Toast.makeText(this@MainActivity, "Invalid URL!", Toast.LENGTH_SHORT).show()
+                        return
                     }
+
+//                    if (jwtToken.isEmpty() || jwtToken.length < 10) {
+//                        Toast.makeText(this@MainActivity, "Invalid JWT Token!", Toast.LENGTH_SHORT).show()
+//                        return
+//                    }
+
+                    if (userInfo.isEmpty()) {
+                        Toast.makeText(this@MainActivity, "Invalid User Info!", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    try {
+                        user = User.fromJson(userInfo)
+                    } catch (e: Exception) {
+                        Toast.makeText(this@MainActivity, "Invalid User Info!", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    startDeskPro()
                 }
 
                 btnOpenMessengerNewChat -> {
-                    messenger = DeskPro(MessengerConfig(appUrl = appUrl, ""))
-                    messenger?.initialize(applicationContext)
-                    messenger?.present()?.show()
-                    user?.let {
-                        messenger?.setUserInfo(it)
-                    }
+                    startDeskPro()
                 }
             }
         }
     }
 
     private fun initListeners() {
-        binding.btnUrl.setOnClickListener(this)
-        binding.btnJWT.setOnClickListener(this)
-        binding.btnUserInfo.setOnClickListener(this)
         binding.btnNotifications.setOnClickListener(this)
         binding.btnOpenMessenger.setOnClickListener(this)
         binding.btnOpenMessengerNewChat.setOnClickListener(this)
+    }
+
+    private fun startDeskPro() {
+        messenger = DeskPro(MessengerConfig(appUrl = appUrl, appId = appId))
+        messenger?.initialize(applicationContext)
+
+        messenger?.authorizeUser(jwtToken)
+
+        messenger?.setUserInfo(user!!)
+
+        messenger?.present()?.show()
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Notification permission granted!")
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                Log.w(TAG, "Notification permission denied permanently!")
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "DeskPro"
     }
 }
