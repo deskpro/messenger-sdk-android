0cb1d7fc4265a951b07187bc5b9b40ccccec2a06
Nikola Divljan <nikola.divljan@qsd.ba>
2024-02-16 09:25:49 +0100

    DP-50 Add more checks to desk pro tests

--- messenger/src/androidTest/java/com/deskpro/messenger/DeskProSetupTest.kt
+++ messenger/src/androidTest/java/com/deskpro/messenger/DeskProSetupTest.kt
@@ -18,12 +18,13 @@ open class DeskProSetupTest {
     private lateinit var context: Context
     private lateinit var messengerConfig: MessengerConfig
     private lateinit var deskPro: DeskPro
+    private val appUrl = "https://dev-pr-12730.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d"
 
     @Before
     fun setUp() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
         messengerConfig = MessengerConfig(
-            appUrl = "https://dev-pr-13019.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d",
+            appUrl = appUrl,
             appId = "",
         )
         deskPro = DeskPro(messengerConfig)
@@ -31,7 +32,7 @@ open class DeskProSetupTest {
     }
 
     @Test
-    fun testUserInfo() {
+    fun testSetAndGetUserInfo() {
         val user = User(
             name = "John Doe",
             firstName = "John",
@@ -40,11 +41,15 @@ open class DeskProSetupTest {
         )
         deskPro.setUserInfo(user)
 
-        Assert.assertEquals("❌The users do not match.❌", user.toJson(), deskPro.getUserInfo()?.toJson())
+        Assert.assertEquals(
+            "❌The users do not match.❌",
+            user.toJson(),
+            deskPro.getUserInfo()?.toJson()
+        )
     }
 
     @Test
-    fun testJwtToken() {
+    fun testSetAndGetJwtToken() {
         val jwtToken = "test_token"
         deskPro.authorizeUser(jwtToken)
 
@@ -52,72 +57,19 @@ open class DeskProSetupTest {
     }
 
     @Test
-    fun testPushRegistrationToken() {
+    fun testSetAndGetPushRegistrationToken() {
         val deviceToken = "test_device_token"
         deskPro.setPushRegistrationToken(deviceToken)
 
-        Assert.assertEquals("❌The tokens do not match.❌", deviceToken, deskPro.getPushRegistrationToken())
-    }
-
-    @Test
-    fun testMultipleDeskProInstances() {
-        val messengerConfig1 = MessengerConfig(
-            appUrl = "https://dev-pr-13019.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d",
-            appId = "1",
-        )
-        val deskPro1 = DeskPro(messengerConfig1)
-        deskPro1.initialize(context)
-
-        deskPro1.setUserInfo(
-            User(
-                name = "John Doe 1",
-                firstName = "John 1",
-                lastName = "Doe 1",
-                email = "john.doe1@email.com"
-            )
-        )
-        deskPro1.authorizeUser("test_token_1")
-        deskPro1.setPushRegistrationToken("test_device_token_1")
-
-        val messengerConfig2 = MessengerConfig(
-            appUrl = "https://dev-pr-13019.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d",
-            appId = "2",
-        )
-        val deskPro2 = DeskPro(messengerConfig2)
-        deskPro2.initialize(context)
-
-        deskPro2.setUserInfo(
-            User(
-                name = "John Doe 2",
-                firstName = "John 21",
-                lastName = "Doe 2",
-                email = "john.doe2@email.com"
-            )
-        )
-        deskPro2.authorizeUser("test_token_2")
-        deskPro2.setPushRegistrationToken("test_device_token_2")
-
-        Assert.assertNotEquals("❌The users match.❌", deskPro1.getUserInfo()?.toJson(), deskPro2.getUserInfo()?.toJson())
-        Assert.assertNotEquals("❌The jwt tokens match.❌", deskPro1.getJwtToken(), deskPro2.getJwtToken())
-        Assert.assertNotEquals("❌The device tokens match.❌", deskPro1.getPushRegistrationToken(), deskPro2.getPushRegistrationToken())
-    }
-
-    @Test
-    fun testPresentBuilder() {
-        val presentBuilder = PresentBuilder("https://dev-pr-13019.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d", "1")
-        presentBuilder.chatHistory(1)
-        presentBuilder.article(1)
-        presentBuilder.comments()
-
         Assert.assertEquals(
-            "❌The paths do not match.❌",
-            "https://dev-pr-13019.earthly.deskprodemo.com/deskpro-messenger/deskpro/1/d/chat_history/1/article/1/comments",
-            presentBuilder.getPath()
+            "❌The tokens do not match.❌",
+            deviceToken,
+            deskPro.getPushRegistrationToken()
         )
     }
 
     @Test
-    fun testClearData() {
+    fun testConfigClearData() {
         deskPro.setUserInfo(
             User(
                 name = "John Doe",
@@ -131,9 +83,117 @@ open class DeskProSetupTest {
 
         deskPro.forgetUser()
 
-        Assert.assertNull("❌The user info is not nil.❌", deskPro.getUserInfo()?.toJson())
+        Assert.assertNull("❌The user info is not nil.❌", deskPro.getUserInfo())
         Assert.assertNull("❌The jwt token is not nil.❌", deskPro.getJwtToken())
         Assert.assertNull("❌The device token is not nil.❌", deskPro.getPushRegistrationToken())
     }
 
+    @Test
+    fun testSetAndGetMultipleDeskProInstances() {
+        //set first instance config data
+        val messengerConfig1 = MessengerConfig(
+            appUrl = appUrl,
+            appId = "1",
+        )
+        val user1 = User(
+            name = "John Doe 1",
+            firstName = "John 1",
+            lastName = "Doe 1",
+            email = "john.doe1@email.com"
+        )
+        val jwtToken1 = "test_token_1"
+        val fcmToken1 = "test_device_token_1"
+
+        val deskPro1 = DeskPro(messengerConfig1)
+        deskPro1.initialize(context)
+
+        deskPro1.setUserInfo(user1)
+        deskPro1.authorizeUser(jwtToken1)
+        deskPro1.setPushRegistrationToken(fcmToken1)
+
+        //set second instance config data
+        val messengerConfig2 = MessengerConfig(
+            appUrl = appUrl,
+            appId = "2",
+        )
+        val user2 = User(
+            name = "John Doe 2",
+            firstName = "John 2",
+            lastName = "Doe 2",
+            email = "john.doe2@email.com"
+        )
+        val jwtToken2 = "test_token_2"
+        val fcmToken2 = "test_device_token_2"
+
+        val deskPro2 = DeskPro(messengerConfig2)
+        deskPro2.initialize(context)
+
+        deskPro2.setUserInfo(user2)
+        deskPro2.authorizeUser(jwtToken2)
+        deskPro2.setPushRegistrationToken(fcmToken2)
+
+        Assert.assertEquals(
+            "❌The users don't match.❌",
+            user1.toJson(),
+            deskPro1.getUserInfo()?.toJson()
+        )
+        Assert.assertEquals(
+            "❌The users don't match.❌",
+            user2.toJson(),
+            deskPro2.getUserInfo()?.toJson()
+        )
+
+
+        Assert.assertEquals("❌The jwt tokens don't match.❌", jwtToken1, deskPro1.getJwtToken())
+        Assert.assertEquals("❌The jwt tokens don't match.❌", jwtToken2, deskPro2.getJwtToken())
+
+        Assert.assertEquals(
+            "❌The device tokens don't match.❌",
+            fcmToken1,
+            deskPro1.getPushRegistrationToken()
+        )
+        Assert.assertEquals(
+            "❌The device tokens don't match.❌",
+            fcmToken2,
+            deskPro2.getPushRegistrationToken()
+        )
+    }
+
+    @Test
+    fun testPresentBuilder() {
+        val presentBuilder = PresentBuilder(
+            appUrl,
+            "1"
+        )
+
+        Assert.assertEquals(
+            "❌The paths do not match.❌",
+            appUrl,
+            presentBuilder.getPath()
+        )
+
+        presentBuilder.chatHistory(1)
+
+        Assert.assertEquals(
+            "❌The paths do not match.❌",
+            "$appUrl/chat_history/1",
+            presentBuilder.getPath()
+        )
+
+        presentBuilder.article(1)
+
+        Assert.assertEquals(
+            "❌The paths do not match.❌",
+            "$appUrl/chat_history/1/article/1",
+            presentBuilder.getPath()
+        )
+
+        presentBuilder.comments()
+
+        Assert.assertEquals(
+            "❌The paths do not match.❌",
+            "$appUrl/chat_history/1/article/1/comments",
+            presentBuilder.getPath()
+        )
+    }
 }
